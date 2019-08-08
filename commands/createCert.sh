9ff83674ad04456711d7dcc63b57462bab7f8904
Scott Brightwell <scott@brightwell.org>
2019-08-07 19:01:29 -0500

    refactoring GCP

---
+++ commands/createCert.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# --------------------------------------------------
+# Create a PCF wildcard certificate based on a domain name passed as an argument 
+# Requires: the command openssl in the path
+# --------------------------------------------------
+
+if [ $# -lt 4 ] ; then
+        echo "Usage: $0 Domain_Name Private_Key Certificate Cert_Configuration"
+        echo ""
+        echo "where:"
+        echo "     Domain_Name: the parent domain from which the PCF subdomains and wildcards will originate"
+        echo "     Private_Key: the file into which the Cert private key will be stored"
+        echo "     Certificate: the file into which the Certificate will be stored"
+        echo "     Cert_Configuration: the file into which the configuration will be stored that generates the Cert"
+        echo ""
+        exit 1
+fi
+
+PCF_DOMAIN_NAME="${1}"
+PRIV_KEY="${2}"
+CERT="${3}"
+CERT_CONFIG="${4}"
+
+cat > ${CERT_CONFIG} <<-EOF
+[req]
+default_bits = 2048
+prompt = no
+default_md = sha256
+req_extensions = req_ext
+distinguished_name = dn
+
+[ dn ]
+C=US
+ST=California
+L=San Francisco
+O=PIVOTAL, INC.
+OU=Workshops
+CN = ${PCF_DOMAIN_NAME}
+
+[ req_ext ]
+subjectAltName = @alt_names
+
+[ alt_names ]
+DNS.1 = *.sys.${PCF_DOMAIN_NAME}
+DNS.2 = *.login.sys.${PCF_DOMAIN_NAME}
+DNS.3 = *.uaa.sys.${PCF_DOMAIN_NAME}
+DNS.4 = *.apps.${PCF_DOMAIN_NAME}
+DNS.5 = *.pks.${PCF_DOMAIN_NAME}
+EOF
+
+openssl req -x509 \
+  -newkey rsa:2048 \
+  -nodes \
+  -keyout ${CERT_PRIV_KEY} \
+  -out ${CERT} \
+  -config ${CERT_CONFIG}
+
+}
--- omsetup.sh
+++ omsetup.sh
@@ -1,169 +1,57 @@
 #!/bin/bash
+set -e
+
+export SCRIPTNAME="$0"
+export SCRIPTDIR=$(cd $(dirname $0) && pwd)
+source "$SCRIPTDIR/common"
 
 DEBUG=0
-DEBUGFILE="initialize_iaas.log"
+LOGFILE="$SCRIPTDIR/$0.log"
 
-# A little function to ask a user for input
-# always returns 0, but echo's the user's sanitized response for capture as a string by the caller
-askUser() {
-	read -p "$1"": " __val
-	# first, strip underscores
-	CLEAN=${__val//_/}
-	# next, replace spaces with underscores
-	CLEAN=${CLEAN// /_}
-	# now, clean out anything that's not alphanumeric or an underscore or a hyphen
-	CLEAN=${CLEAN//[^a-zA-Z0-9_-]/}
-	echo "${CLEAN}"
 
-	return 0
-}
+# --------------------------------------------------
+# Create a PCF wildcard certificate based on a domain name passed as an argument
+# Requires: the command openssl in the path
+# --------------------------------------------------
 
-# A little more function based on askUser() to specifically ask a yes or no question
-# returns 0 for Yes or 1 for "not yes"
-askYes() {
-	CLEAN="$(askUser "$1 (Y|y) ")"
-	if [ "$CLEAN" == "${CLEAN#[Yy]}" ]; then
-		return 1
-	else
-		return 0
-	fi
-}	
+if [ $# -lt 4 ] ; then
+        echo "usage: $0 IaaS domain environment pivnet-token"
+        echo ""
+        echo "where:"
+        echo "     IaaS: is one of {gcp | aws | azure | vsphere}"
+        echo "     domain: the parent domain from which the PCF subdomains and wildcards will originate"
+        echo "     environment: the short name for the environment that will be the subdomain for PCF"
+        echo "     	Example:  domain=example.com & environment=test results in pcf.test.example.com"
+        echo "     pivnet-token: the LEGACY API TOKEN [DEPRECATED] from your user profile on network.pivotal.io"
+        echo ""
+        exit 1
+fi
+
+#OM_IAAS="$(askUser "Please pick a target IAAS: gcp, aws, azure, or vsphere? ")"
+OM_IAAS="$1"
+OM_DOMAIN_NAME="$2"
+#OM_ENV_NAME="$(askUser "Decide on a short subdomain name (like \"pcf\") for the environment? ")"
+OM_ENV_NAME="$3"
+#PIVNET_API_TOKEN="iigMJxjc3wkqxRiknHR1"
+PIVNET_API_TOKEN="$4"
 
 # TODO set these dynamically
-PIVNET_API_TOKEN="iigMJxjc3wkqxRiknHR1"
 OM_ADMIN_USERNAME="admin"
 
-OM_IAAS="$(askUser "Please pick a target IAAS: gcp, aws, azure, or vsphere? ")"
-OM_DOMAIN_NAME="${OM_IAAS}.harnessingunicorns.io"
-OM_ENV_NAME="$(askUser "Decide on a short subdomain name (like \"pcf\") for the environment? ")"
 OM_STATE_DIRECTORY="$PWD/state/$OM_IAAS/$OM_ENV_NAME"
+mkdir -p "$OM_STATE_DIRECTORY"
+
 OM_ENVIRONMENT_VARS="$OM_STATE_DIRECTORY/$OM_ENV_NAME.envrc"
 if [[ -f "${OM_ENVIRONMENT_VARS}" ]]; then
     source "${OM_ENVIRONMENT_VARS}"
 fi
 
-mkdir -p "$OM_STATE_DIRECTORY"
-
 OM_CERT_PRIV_KEY="${OM_STATE_DIRECTORY}/${PCF_DOMAIN_NAME}.key"
 OM_CERT="${OM_STATE_DIRECTORY}/${PCF_DOMAIN_NAME}.cert"
 OM_CERT_CONFIG="${OM_STATE_DIRECTORY}/${PCF_DOMAIN_NAME}.cnf"
 
-# Create a PCF wildcard certificate based on a domain name passed as an argument 
-# Requires: openssl in the path
-function createCert()
-{
-	PCF_DOMAIN_NAME=${1}
-	cat > ${OM_CERT_CONFIG} <<-EOF
-[req]
-default_bits = 2048
-prompt = no
-default_md = sha256
-req_extensions = req_ext
-distinguished_name = dn
 
-[ dn ]
-C=US
-ST=California
-L=San Francisco
-O=PIVOTAL, INC.
-OU=Workshops
-CN = ${PCF_DOMAIN_NAME}
-
-[ req_ext ]
-subjectAltName = @alt_names
-
-[ alt_names ]
-DNS.1 = *.sys.${PCF_DOMAIN_NAME}
-DNS.2 = *.login.sys.${PCF_DOMAIN_NAME}
-DNS.3 = *.uaa.sys.${PCF_DOMAIN_NAME}
-DNS.4 = *.apps.${PCF_DOMAIN_NAME}
-DNS.5 = *.pks.${PCF_DOMAIN_NAME}
-EOF
-
-openssl req -x509 \
-  -newkey rsa:2048 \
-  -nodes \
-  -keyout ${OM_CERT_PRIV_KEY} \
-  -out ${OM_CERT} \
-  -config ${OM_CERT_CONFIG}
-
-}
-
-function gcpInitialize()
-{
-	OM_GCP_SERVICE_ACCOUNT_KEY="${OM_GCP_SERVICE_ACCOUNT_KEY:-UNSET}"
-	OM_GCP_REGION="${OM_GCP_REGION:-UNSET}"
-	
-	echo "Here are how the required environment variables to bbl up on $OM_IAAS are currently set:"
-	echo "OM_IAAS=$OM_IAAS"
-	echo "OM_ENV_NAME=$OM_ENV_NAME"
-	echo "OM_GCP_REGION=$OM_GCP_REGION"
-	echo "OM_GCP_SERVICE_ACCOUNT_KEY=$OM_GCP_SERVICE_ACCOUNT_KEY"
-	echo "OM_STATE_DIRECTORY=$OM_STATE_DIRECTORY"
-		
-	echo ""; askYes "Would you like to continue and get help populating these values?"; RETVAL=$?
-	if [[ $RETVAL -eq 1 ]]; then echo "Ok then.  Good luck bbl-ing up on $OM_IAAS!"; return ${RETVAL}; fi
-
-	sleep 1; echo ""; echo "Great!  Let's continue."
-	echo ""; echo "Let's determine your GCP Project ID"
-	echo "gcloud projects list"
-	GCP_PROJECTS="`gcloud projects list | grep -v "PROJECT_NUMBER" | awk '{print $1}'`"
-	if [ `echo $GCP_PROJECTS | wc -w` == "1" ]; then
-		GCP_PROJECT_ID=$GCP_PROJECTS
-		echo "Since you only have access to a single GCP project.  We'll use it for this deployment"
-		echo "GCP_PROJECT_ID=$GCP_PROJECT_ID"
-	else
-		echo "You seem to have access to these GCP projects:"
-		echo "$GCP_PROJECTS"
-		GCP_PROJECT_ID="$(askUser "Please enter one of the above GCP Project ID's for this deployment")"
-	fi
-
-	echo ""; echo "Creating a new service account in GCP that will own the BOSH deployment"
-	GCP_SERVICE_ACCOUNT_NAME="`echo $OM_ENV_NAME | awk '{print tolower($0)}'`""serviceaccount"
-	echo "$ gcloud iam service-accounts create $GCP_SERVICE_ACCOUNT_NAME"
-	gcloud iam service-accounts create $GCP_SERVICE_ACCOUNT_NAME
-	RETVAL=$?
-	if [[ $RETVAL -eq 1 ]]; then echo "Hmmm.  You may need to execute a \"gcloud init\" if you're having issues with permissions."; exit 1; fi
-	
-	sleep 1; echo ""; echo "Now I need to create a service account key. I'll store it here:"
-	OM_GCP_SERVICE_ACCOUNT_KEY="$OM_STATE_DIRECTORY/terraform.key.json"
-	echo "$OM_GCP_SERVICE_ACCOUNT_KEY"
-	touch $OM_GCP_SERVICE_ACCOUNT_KEY;  chmod 700 $OM_GCP_SERVICE_ACCOUNT_KEY
-	echo "$ gcloud iam service-accounts keys create --iam-account=\"${GCP_SERVICE_ACCOUNT_NAME}@${GCP_PROJECT_ID}.iam.gserviceaccount.com\" $OM_GCP_SERVICE_ACCOUNT_KEY"
-	askYes "Are you good with me issuing the above command?"; RETVAL=$?
-	if [[ $RETVAL -eq 1 ]]; then echo "Bailing out now!  Good luck bbl-ing up on $OM_IAAS!"; exit 1; fi
-	gcloud iam service-accounts keys create --iam-account="${GCP_SERVICE_ACCOUNT_NAME}@${GCP_PROJECT_ID}.iam.gserviceaccount.com" $OM_GCP_SERVICE_ACCOUNT_KEY
-	RETVAL=$?
-	if [[ $RETVAL -eq 1 ]]; then echo "Hmmm.  You may need to execute a \"gcloud init\" if you're having issues with permissions."; exit 1; fi
-
-
-	sleep 1; echo ""; echo "Binding the service account to the project with owner role"
-	echo "gcloud projects add-iam-policy-binding ${GCP_PROJECT_ID} --member=\"serviceAccount:${GCP_SERVICE_ACCOUNT_NAME}@${GCP_PROJECT_ID}.iam.gserviceaccount.com\" --role='roles/owner'"
-	askYes "Are you good with me issuing the above command?"; RETVAL=$?
-	if [[ $RETVAL -eq 1 ]]; then echo "Bailing out now!  Good luck bbl-ing up on $OM_IAAS!"; exit 1; fi
-	gcloud projects add-iam-policy-binding ${GCP_PROJECT_ID} --member="serviceAccount:${GCP_SERVICE_ACCOUNT_NAME}@${GCP_PROJECT_ID}.iam.gserviceaccount.com" --role='roles/owner'
-	RETVAL=$?
-	if [[ $RETVAL -eq 1 ]]; then echo "Hmmm.  You may need to execute a \"gcloud init\" if you're having issues with permissions."; exit 1; fi
-
-	sleep 1; echo ""; echo "Here is a list of regions where BOSH can be deployed"
-	echo "$ gcloud compute regions list"
-	gcloud compute regions list
-	OM_GCP_REGION="$(askUser "Please input the name of one of these regions for the deployment")"
-
-
-	echo ""; echo "Here are the environment variables you need to set to deploy BOSH on $OM_IAAS:"
-	echo ""
-	{
-	echo "export OM_IAAS=$OM_IAAS"
-	echo "export OM_ENV_NAME=$OM_ENV_NAME"
-	echo "export OM_STATE_DIRECTORY=$OM_STATE_DIRECTORY"
-	echo "export OM_GCP_REGION=$OM_GCP_REGION"
-	echo "export OM_GCP_SERVICE_ACCOUNT_KEY=$OM_GCP_SERVICE_ACCOUNT_KEY"
-	echo "export GCP_PROJECT_ID=$GCP_PROJECT_ID"
-	echo "export GCP_SERVICE_ACCOUNT_NAME=$GCP_SERVICE_ACCOUNT_NAME"
-	} | tee $OM_ENVIRONMENT_VARS
-}
-
+# --------------------------------------------------
 function azureInitialize()
 {
 	OM_AZURE_REGION="${OM_AZURE_REGION:-UNSET}"
@@ -207,7 +95,7 @@ function azureInitialize()
 
 	export OM_AZURE_TENANT_ID="`echo \"$AZ_ACCOUNT_LIST\" | jq -r '.[] | select(.isDefault) | .tenantId'`"
 	echo "Your OM_AZURE_TENANT_ID is $OM_AZURE_TENANT_ID"
-	if [ $DEBUG ]; then echo "$AZ_ACCOUNT_LIST" >> $DEBUGFILE; fi
+	if [ $DEBUG ]; then echo "$AZ_ACCOUNT_LIST" >> $LOGFILE; fi
 	
 
 	AZURE_SP_DISPLAY_NAME="Service Principal for BOSH"
@@ -226,14 +114,14 @@ function azureInitialize()
 		--identifier-uris \"$AZURE_SP_IDENTIFIER_URI\"`"
 	export OM_AZURE_CLIENT_ID="`echo \"$AZ_AD_APP_CREATE\" | jq -r '.appId'`"
 	echo "Your OM_AZURE_CLIENT_ID is $OM_AZURE_CLIENT_ID"
-	if [ $DEBUG ]; then echo "$AZ_AD_APP_CREATE" >> $DEBUGFILE; fi
+	if [ $DEBUG ]; then echo "$AZ_AD_APP_CREATE" >> $LOGFILE; fi
 
 	echo ""; echo "Creating the Service Principal corresponding to the new Application"
 	echo "$ az ad sp create --id $OM_AZURE_CLIENT_ID"
 	askYes "Are you good with me issuing the above command?"; RETVAL=$?
 	if [[ $RETVAL -eq 1 ]]; then echo "Bailing out now!  Good luck bbl-ing up on $OM_IAAS!"; exit 1; fi
 	AZ_AD_SP_CREATE="`az ad sp create --id $OM_AZURE_CLIENT_ID`"
-	if [ $DEBUG ]; then echo "$AZ_AD_SP_CREATE" >> $DEBUGFILE; fi
+	if [ $DEBUG ]; then echo "$AZ_AD_SP_CREATE" >> $LOGFILE; fi
 
 	echo ""; echo "Sleeping 45 seconds to let Azure AD catch up before proceeding"
 	sleep 45
@@ -242,7 +130,7 @@ function azureInitialize()
 	askYes "Are you good with me issuing the above command?"; RETVAL=$?
 	if [[ $RETVAL -eq 1 ]]; then echo "Bailing out now!  Good luck bbl-ing up on $OM_IAAS!"; exit 1; fi
 	AZ_ROLE_ASSIGNMENT_CREATE="`az role assignment create --assignee $OM_AZURE_CLIENT_ID --role Owner --scope /subscriptions/$OM_AZURE_SUBSCRIPTION_ID`"
-	if [ $DEBUG ]; then echo "$AZ_ROLE_ASSIGNMENT_CREATE" >> $DEBUGFILE; fi
+	if [ $DEBUG ]; then echo "$AZ_ROLE_ASSIGNMENT_CREATE" >> $LOGFILE; fi
 
 	echo ""; echo "Registering the Subscription with Microsoft Storage, Network, and Compute"
 	echo "$ az provider register --namespace Microsoft.Storage"
@@ -270,10 +158,9 @@ function azureInitialize()
 	} | tee $OM_ENVIRONMENT_VARS
 }
 
+# --------------------------------------------------
 function omDeploy()
 {
-	sleep 1; echo ""; echo "Creating a self signed certificate for use in the deployment"
-	createCert ${OM_DOMAIN_NAME}
 
 	echo "Downloading Terraform templates from Pivnet to pave $OM_IAAS"
 	PIVNET_FILE_GLOB="terraforming-${OM_IAAS}*zip"
@@ -347,6 +234,7 @@ fi
 	terraform apply plan
 }
 
+# --------------------------------------------------
 function azureConfigure()
 {
 	OM_ADMIN_USER="admin"
@@ -415,6 +303,7 @@ function azureConfigure()
 
 }
 
+# --------------------------------------------------
 function gcpConfigure()
 {
 	OM_ADMIN_USER="admin"
@@ -493,7 +382,11 @@ function gcpConfigure()
 }
 
 if [ $OM_IAAS == "gcp" ]; then
-	gcpInitialize
+	${SCRIPTDIR}/commands/gcpInitialize.sh "${OM_STATE_DIRECTORY}" "${OM_ENVIRONMENT_VARS}"
+	sleep 1; echo ""; echo "Creating a self signed certificate for use in the deployment"
+	${SCRIPTDIR}/commands/createCert.sh "${OM_DOMAIN_NAME}" "${OM_CERT_PRIV_KEY}" "${OM_CERT}" "${OM_CERT_CONFIG}" 
+	echo "exiting" 
+	exit 0
 	omDeploy
 	gcpConfigure
 elif [ $OM_IAAS == "aws" ]; then
@@ -511,18 +404,3 @@ else
 	exit 1
 fi
 
-exit
-echo "";echo ""
-echo "Now your IAAS should be set and ready to use bbl to create a bosh environment."
-echo ""; echo "What you should do now:"
-echo "# The following will enable the bbl command by sourcing the required env vars"
-echo "source $OM_ENVIRONMENT_VARS"
-echo "bbl -h                        # familiarize yourself with bbl generally"
-echo "bbl plan -h                    # familiarize yourself with the bbl plan command line options"
-echo "bbl up -h                    # familiarize yourself with the bbl up command line options"
-echo "bbl plan --lb-type concourse --debug        # this will create lots of structures in \$OM_STATE_DIRECTORY"
-echo "";echo "# after executing bbl plan, now you can go into \$OM_STATE_DIRECTORY and make edits and customizations"
-echo "";echo "bbl up --lb-type concourse --debug        # this will execute the plan with customizations on your IAAS"
-
-echo "";echo ""
-echo "Checkout the README.md for additional next steps.  Thanks for using bblwrap!"
